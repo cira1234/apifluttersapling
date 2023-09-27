23e75d2f34277c129afcdae2bb797faa612b5aa5
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-09-27 20:44:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get('/selectmember',(req,res) =>{
      app.get('/selectprofile/:iduser',(req,res) =>{
            var iduser=req.params.iduser;
     connection.query(
-        'SELECT * FROM user  WHERE id_user=? ',[iduser],
+        'SELECT * FROM imguser  WHERE id_imguser=? ',[iduser],
     
         function(err,results,fields){
             console.log(results)
