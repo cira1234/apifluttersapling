acff10884a2333286cfdc789a485d89022fb2c90
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-11-25 15:58:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -334,7 +334,7 @@ app.get('/selectmember',(req,res) =>{
         app.get('/selectpostreport',(req,res) =>{
                 
     connection.query(
-        'select * from post where report>=? ',['1'],
+        'select * from post where report>=? and img!=? ',['1','default.jpg'],
     
         function(err,results,fields){
             console.log(results)
