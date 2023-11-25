057016309c1d9685dd9de099616ce2165ad86a3d
cira1234 <77906052+cira1234@users.noreply.github.com>
2023-11-25 19:01:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -148,6 +148,21 @@ app.get('/selectmember',(req,res) =>{
 }),
 
 
+     app.get('/selectvideosearch/:idtype',(req,res) =>{
+          var idtype=req.params.idtype;
+    connection.query(
+        'select * from videostudy inner join typesearch on typevideo = id_type where typesearch=?',[idtype],
+    
+        function(err,results,fields){
+            console.log(results)
+            //res.send(results)
+            res.send(results);
+            //    res.send({"msg":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"})
+        }
+    )
+}),
+
+
     app.get('/selectmember',(req,res) =>{
     connection.query(
         'select * from member ',
