15d835444ea9a6e3a12c6f60ac0830d203fb485b
cira1234 <77906052+cira1234@users.noreply.github.com>
2024-01-16 16:44:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.get('/selectmember',(req,res) =>{
                var story=req.params.story;
           var idquiz=req.params.idquiz;
     connection.query(
-        'select *  from quiz inner join listquiz on listquiz.id = quiz.storyname WHERE  quiz.storyname =?',[story,idquiz],
+        'select *  from quiz inner join listquiz on listquiz.storyname = quiz.storyname WHERE  quiz.storyname=?',[story,idquiz],
     
         function(err,results,fields){
             if(results.length==0){
