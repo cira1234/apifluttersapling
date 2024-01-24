4bd0199f6988e6d3f96ef31eb8f9a0728a64e9f0
cira1234 <77906052+cira1234@users.noreply.github.com>
2024-01-25 01:04:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -221,7 +221,7 @@ app.get('/selectmember',(req,res) =>{
 }),
 
 
-          app.get('/selectwritequiz/:story/:idquiz',(req,res) =>{
+          app.get('/selectwritequiz/:story',(req,res) =>{
                var story=req.params.story;
           var idquiz=req.params.idquiz;
     connection.query(
